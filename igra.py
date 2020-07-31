bb9fb37f6ddd611370054c3ba58d6342d59ae385
ZlataS <68561126+ZlataS@users.noreply.github.com>
2020-08-01 00:49:29 +0200

    Add files via upload

--- igra.py
+++ igra.py
@@ -6,24 +6,46 @@ pozadina = (64, 64, 64)
 boje = {0:(74, 74, 74),2:(255,224,153),4:(255,158,102),8:(255, 100, 71),16:(246, 189, 0),32:(100,60,10),64:(200,0,0),128:(111,53,70),256:(50,200,200),512:(125,75,199),1024:(144,10,40),2048:(255,255,180)}
 tabla = []
 prtabla = []
-pomerajdesno=[]
-pomerajdole=[]
 VelicinaTable = 4 #moze da se menja velicina table, sada je 4x4
-for i in range (VelicinaTable):
-    tabla.append([])
-    pomerajdesno.append([])
-    pomerajdole.append([])
-    for j in range(VelicinaTable):
-        tabla[i].append(0)
-        pomerajdesno[i].append(0)
-        pomerajdole[i].append(0)
 (sirinapolja,visinapolja)=(60,60) #mogu da se menjaju dimenzije polja
 margina = 10 #moze da se menja sirina margine
-dimenzijeprozora = [sirinapolja*VelicinaTable+margina*(VelicinaTable+1), visinapolja*VelicinaTable+margina*(VelicinaTable+1)]
+def prvaigra():
+    global dimenzijeprozora
+    global tabla
+    global prozor
+    global prtabla
+    for i in range(VelicinaTable):
+        tabla.append([])
+        for j in range(VelicinaTable):
+            tabla[i].append(0)
+    dimenzijeprozora = [sirinapolja * VelicinaTable + margina * (VelicinaTable + 1),
+                        visinapolja * VelicinaTable + margina * (VelicinaTable + 1)]
+    prozor = pg.display.set_mode(dimenzijeprozora)
+    slpolje()
+    slpolje()
+    prtabla = copy.deepcopy(tabla)
+    CrtajTablu()
 pg.init()
-prozor = pg.display.set_mode(dimenzijeprozora)
+prozor = pg.display.set_mode([280,320])
 pg.display.set_caption("2048")
-
+prozor.fill(pozadina)
+font = pg.font.SysFont('boulder', 40)
+tekst = font.render("Izaberi tablu:", True, (255, 255, 255))
+prozor.blit(tekst, (50, 20))
+pg.draw.rect(prozor,(255,158,102),[20, 60, 110, 110])
+pg.draw.rect(prozor,(255,158,102),[150, 60, 110, 110])
+pg.draw.rect(prozor,(255,158,102),[20, 190, 110, 110])
+pg.draw.rect(prozor,(255,158,102),[150, 190, 110, 110])
+font = pg.font.SysFont('boulder', 40)
+tekst = font.render("3x3", True, (255, 255, 255))
+prozor.blit(tekst, (50, 100))
+tekst = font.render("4x4", True, (255, 255, 255))
+prozor.blit(tekst, (180, 100))
+tekst = font.render("5x5", True, (255, 255, 255))
+prozor.blit(tekst, (50, 230))
+tekst = font.render("6x6", True, (255, 255, 255))
+prozor.blit(tekst, (180, 230))
+pg.display.update()
 sledecepolje = [2,2,2,4] #posto u igrici mogu da se pojave i dvojke i cetvorke, ali redje cetvorke?
 def slpolje():
     global tabla
@@ -123,15 +145,21 @@ izgubio_si = False
 pobedio_si = False
 font = pg.font.SysFont('boulder', 20)
 tekst2 = font.render("pritisni SPACE da pokrenes novu partiju", True, (255, 255, 255))
-slpolje()
-slpolje()
-prtabla = copy.deepcopy(tabla)
-CrtajTablu()
 done = False
 while not done:
     for dogadjaj in pg.event.get():
         if dogadjaj.type == pg.QUIT:
             done = True
+        elif dogadjaj.type == pg.MOUSEBUTTONDOWN:
+            (mis_x,mis_y) = pg.mouse.get_pos()
+            if mis_x>=140 and mis_y>=180:
+                VelicinaTable = 6
+            elif mis_x>=140 and mis_y<180:
+                VelicinaTable = 4
+            elif mis_x<140 and mis_y<180:
+                VelicinaTable = 3
+            else: VelicinaTable = 5
+            prvaigra()
         elif dogadjaj.type == pg.KEYDOWN:
             mogucpotez = True
             if dogadjaj.key == pg.K_SPACE:
@@ -140,8 +168,6 @@ while not done:
                 for i in range(VelicinaTable):
                     for j in range(VelicinaTable):
                         tabla[i][j] = 0
-                        pomerajdesno[i][j] = 0
-                        pomerajdole[i][j] = 0
                 slpolje()
                 slpolje()
                 CrtajTablu()
@@ -150,7 +176,7 @@ while not done:
                 tekst = font.render("PORAZ", True, (255, 255, 255))
                 prozor.fill(pozadina)
                 prozor.blit(tekst, ((VelicinaTable-4)*((sirinapolja+margina)//2)+19, dimenzijeprozora[1]//2-45))
-                prozor.blit((VelicinaTable-4)*((sirinapolja+margina)//2)+20, (20, dimenzijeprozora[1]//2+20))
+                prozor.blit(tekst2, (VelicinaTable-4)*((sirinapolja+margina)//2)+20, (20, dimenzijeprozora[1]//2+20))
                 pg.display.update()
             elif pobedio_si:
                 font = pg.font.SysFont('boulder', 100)
